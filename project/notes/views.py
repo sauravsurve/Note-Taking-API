9cbff4d37ff32890059507230c9a870b06111888
Saurav Surve <sau97.surve@gmail.com>
2020-03-05 10:20:18 +0530

    Updated Views.py
    Basic Authentication added to views.py

--- project/notes/views.py
+++ project/notes/views.py
@@ -14,10 +14,16 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 
+#Authentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
+
 
 from datetime import date
 
 class NoteViewSet(viewsets.ModelViewSet):
+	permission_classes=(IsAuthenticated,)
+	authentication_classes = (BasicAuthentication,)
 	serializer_class = NoteSerializer
 	queryset = Note.objects.all()
 
